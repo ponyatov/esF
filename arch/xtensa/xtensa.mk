2f401987adba3f1701347792f5a21c361ebf04b6
Dmitry Ponyatov <dponyatov@gmail.com>
2025-04-20 20:37:56 +0400

    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- arch/xtensa/xtensa.mk
+++ arch/xtensa/xtensa.mk
@@ -1,6 +1,10 @@
 TARGET    = xtensa-lx106-elf
 OS       ?= rtos8266
+
 APT      += gcc-xtensa-lx106 qemu-system-misc
+APT      += python3-future python3-cryptography
+APT      += python3-pyparsing python3-pyelftools python3-pyparsing
+
 QEMU      = qemu-system-xtensa
 QEMU_CFG += -machine esp8266 -nographic
 
